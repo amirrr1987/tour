2782e7ef88b4b25a54614257e76f42314ee150b5
Amir Maghami <Maghami.A1987@gmail.com>
2024-07-31 13:14:51 +0330

    update

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,3 +1,49 @@
+<script setup lang="ts">
+import { object, string, type InferType } from "yup";
+import type { FormSubmitEvent } from "#ui/types";
+import { gsap } from "gsap";
+
+const schema = object({
+  username: string().required("Required"),
+  password: string().required("Required"),
+});
+
+type Schema = InferType<typeof schema>;
+
+const state = reactive({
+  username: undefined,
+  password: undefined,
+});
+
+async function onSubmit(event: FormSubmitEvent<Schema>) {
+  // Do something with event.data
+  console.log(event.data);
+}
+const formRef = ref(null);
+
+onMounted(() => {
+  gsap.from(formRef.value, { y: "-100vh", duration: 1, opacity: 0 });
+  gsap.to(formRef.value, { y: 0, duration: 1, opacity: 1 });
+});
+</script>
+
 <template>
-    <div class=""></div>
+  <div class="h-screen flex items-center justify-center" ref="formRef">
+    <UForm
+      :schema="schema"
+      :state="state"
+      class="space-y-4 w-100"
+      @submit="onSubmit"
+    >
+      <UFormGroup label="Username" name="username">
+        <UInput v-model="state.username" />
+      </UFormGroup>
+
+      <UFormGroup label="Password" name="password">
+        <UInput v-model="state.password" type="password" />
+      </UFormGroup>
+
+      <UButton type="submit"> Submit </UButton>
+    </UForm>
+  </div>
 </template>
--- pages/auth/register.vue
+++ pages/auth/register.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <div class="flex justify-center items-center h-screen">
     <form class="w-100" @submit="register" ref="formRef">
       <img src="" alt="" />
@@ -55,8 +55,8 @@
       </button>
     </form>
   </div>
-</template>
-<script setup lang="ts">
+</template> -->
+<!-- <script setup lang="ts">
 import { gsap } from "gsap";
 
 const state = reactive({
@@ -76,4 +76,67 @@ onMounted(() => {
   gsap.from(formRef.value, { y: "-100vh", duration: 1, opacity: 0 });
   gsap.to(formRef.value, { y: 0, duration: 1, opacity: 1 });
 });
+</script> -->
+
+<script setup lang="ts">
+import { object, string, type InferType } from "yup";
+import type { FormSubmitEvent } from "#ui/types";
+import { gsap } from "gsap";
+
+const schema = object({
+  username: string().required("Required"),
+  password: string().required("Required"),
+  email: string().email().required("Required"),
+});
+
+type Schema = InferType<typeof schema>;
+
+const state = reactive({
+  username: "",
+  password: "",
+  email: "",
+});
+const register = async () => {
+  await useFetch("http://10.0.202.34:8080/api/auth/register", {
+    method: "post",
+    body: state,
+  });
+};
+
+async function onSubmit(event: FormSubmitEvent<Schema>) {
+  // Do something with event.data
+  console.log(event.data);
+  register();
+}
+const formRef = ref(null);
+
+onMounted(() => {
+  gsap.from(formRef.value, { y: "-100vh", duration: 1, opacity: 0 });
+  gsap.to(formRef.value, { y: 0, duration: 1, opacity: 1 });
+});
 </script>
+
+<template>
+  <div class="h-screen flex items-center justify-center" ref="formRef">
+    <UForm
+      :schema="schema"
+      :state="state"
+      class="space-y-4 w-100"
+      @submit.prevent="onSubmit"
+    >
+      <UFormGroup label="email" name="email">
+        <UInput v-model="state.email" />
+      </UFormGroup>
+
+      <UFormGroup label="Username" name="username">
+        <UInput v-model="state.username" />
+      </UFormGroup>
+
+      <UFormGroup label="Password" name="password">
+        <UInput v-model="state.password" type="password" />
+      </UFormGroup>
+
+      <UButton type="submit"> Submit </UButton>
+    </UForm>
+  </div>
+</template>
