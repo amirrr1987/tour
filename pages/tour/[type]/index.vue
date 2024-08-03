f2003ec5986fe04367354dcb2c81f200fc37ff6b
Amir Maghami <Maghami.A1987@gmail.com>
2024-08-03 23:56:16 +0330

    Create index.vue

---
+++ pages/tour/[type]/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <section class="py-12">
+        <AContainer class="grid grid-cols-2 gap-4">
+            <UCard>sdfsfd</UCard>
+            <UCard>sdfsfd</UCard>
+        </AContainer>
+    </section>
+</template>
