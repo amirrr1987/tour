6a702ae661cc9f5ed6d9f27c054d9167191329dd
Amir Maghami <Maghami.A1987@gmail.com>
2024-08-04 14:33:29 +0330

    :sparkles: dynamic url tour data

--- components/The/Header.vue
+++ components/The/Header.vue
@@ -1,23 +1,31 @@
 <script setup lang="ts">
 const inputUi = {
   base: "md:w-96",
-}
-
+};
 
 const buttonGroupUi = {
   rounded: "rounded-xl",
   shadow: "shadow-xl",
 };
+
+const city = ref("");
+
+const router = useRouter();
+const findByData = () => {
+  router.push(`/tour/?city=${city.value}`);
+};
+const cities = ['خوی', 'Arlene Mccoy', 'Devon Webb', 'Tom Cook', 'Tanya Fox', 'Hellen Schmidt', 'Caroline Schultz', 'Mason Heaney', 'Claudie Smitham', 'Emil Schaefer']
 </script>
 
 <template>
   <header class="py-12 flex">
     <AContainer class="flex justify-center items-center">
       <UButtonGroup size="xl" orientation="horizontal" :ui="buttonGroupUi">
-        <UInput :ui="inputUi" />
-        <UButton icon="tabler:click" color="green" />
-        <UButton icon="tabler:x" color="green" />
-        <UButton icon="tabler:search" color="green" />
+        <UInputMenu  :ui="inputUi" v-model="city" :options="cities" @keyup.enter="findByData" />
+
+        <!-- <UButton icon="tabler:click" color="green" /> -->
+        <!-- <UButton icon="tabler:x" color="green" /> -->
+        <UButton icon="tabler:search" color="green" label="جستجو" @click="findByData" />
       </UButtonGroup>
     </AContainer>
   </header>
---
+++ pages/tour/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="py-12">
+    <AContainer class="grid grid-cols-12 gap-4">
+      <UCard class="col-span-2">
+        <UForm>
+          <UFormGroup>
+            <USelectMenu v-model="city" :options="cities" />
+          </UFormGroup>
+        </UForm>
+      </UCard>
+      <div class="col-span-10 flex flex-wrap gap-4">
+        <UCard v-for="item in 30" class="w-[24.02%]">{{ item }}</UCard>
+      </div>
+    </AContainer>
+  </section>
+</template>
+<script setup lang="ts">
+import { useRouteQuery } from "@vueuse/router";
+const cities = ['خوی', 'Arlene Mccoy', 'Devon Webb', 'Tom Cook', 'Tanya Fox', 'Hellen Schmidt', 'Caroline Schultz', 'Mason Heaney', 'Claudie Smitham', 'Emil Schaefer']
+
+definePageMeta({
+  layout: "filter",
+});
+
+const city = useRouteQuery("city", "", { transform: String });
+
+console.log(city.value);
+</script>
