f2b6d389e4bb9831308d3a909df47a7160bee391
Amir Maghami <Maghami.A1987@gmail.com>
2024-08-26 10:46:39 +0330

    Update index.vue

--- pages/tour/index.vue
+++ pages/tour/index.vue
@@ -9,11 +9,11 @@
         </UForm>
       </UCard>
       <div class="col-span-10 flex flex-wrap gap-4">
-        <UCard v-for="(item, index) in tours" :key="index" class="w-[24.02%]">
+        <UCard as="figure" v-for="(item, index) in tours" :key="index" class="w-[24.02%]" :ui="cardUi">
           <template #header>
             <img :src="item.mainImageAddress" :title="item.name" :alt="item.name" >
           </template>
-          {{ item }}</UCard
+          {{ item.name }}</UCard
         >
       </div>
     </AContainer>
@@ -42,4 +42,30 @@ const city = useRouteQuery("city", "", { transform: String });
 const { data: tours } = await useFetch("/api/v1/tour/getAll");
 
 console.log(city.value);
+
+
+const cardUi = {
+  base: '',
+  background: 'bg-white dark:bg-gray-900',
+  divide: 'divide-y divide-gray-200 dark:divide-gray-800',
+  ring: 'ring-1 ring-gray-200 dark:ring-gray-800',
+  rounded: 'rounded-lg',
+  shadow: 'shadow',
+  body: {
+    base: '',
+    background: '',
+    padding: 'px-4 py-5 sm:p-6',
+  },
+  header: {
+    base: '',
+    background: '',
+    padding: 'px-0 py-0 sm:px-0',
+  },
+  footer: {
+    base: '',
+    background: '',
+    padding: 'px-4 py-4 sm:px-6',
+  },
+}
+
 </script>
