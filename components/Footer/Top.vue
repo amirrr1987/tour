6898cb07b6aca1b7746a2a2dc764586f456cace6
Amir Maghami <Maghami.A1987@gmail.com>
2024-09-30 16:12:11 +0330

    Update Top.vue

--- components/Footer/Top.vue
+++ components/Footer/Top.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="py-12">
     <TheContainer class="grid gap-8 md:grid-cols-12">
-      <NuxtLink to="/" class="flex md:col-span-4">
+      <NuxtLink to="/" class="flex md:col-span-2 items-center">
         <NuxtImg
-          src="https://flowbite.com/docs/images/logo.svg"
-          class="h-8 me-3"
+          src="assets/img/logo.png"
+          class="h-16 me-3"
           alt="FlowBite Logo"
         />
-        <span class="text-2xl font-semibold whitespace-nowrap dark:text-white"
-          >فلو بایت</span
-        >
+        <span class="text-2xl font-semibold whitespace-nowrap text-gray-500 dark:text-white">
+          تورکده
+        </span>
       </NuxtLink>
-      <div class="grid grid-cols-2 md:grid-cols-4 gap-8 md:col-span-8">
-        <div v-for="item in 4">
+      <div class="grid grid-cols-2 md:grid-cols-6 gap-8 md:col-span-8">
+        <div v-for="item in 6">
           <h2
             class="mb-6 text-sm font-medium text-gray-900 uppercase dark:text-white"
           >
@@ -21,7 +21,7 @@
           <ul
             class="text-gray-500 dark:text-gray-400 font-medium flex flex-col gap-y-2"
           >
-            <li v-for="single in 4">
+            <li v-for="single in 6">
               <NuxtLink to="https://flowbite.com/" class="hover:underline">
                 فلو بایت
               </NuxtLink>
