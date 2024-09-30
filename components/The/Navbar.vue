b5dbcae0ef928816e230a54fa7b6edae7fe9614b
Amir Maghami <Maghami.A1987@gmail.com>
2024-09-30 16:12:22 +0330

    Update Navbar.vue

--- components/The/Navbar.vue
+++ components/The/Navbar.vue
@@ -6,10 +6,10 @@ const { data } = await useFetch("http://10.0.202.34:8081/category/getAll");
   <nav class="bg-white shadow sticky top-0 z-50 py-4">
     <TheContainer class="flex items-center gap-x-4">
       <ul class="flex gap-4 flex-1">
-        <li><NuxtLink to="/">صفحه اصلی</NuxtLink></li>
-        <li><NuxtLink to="/tour-list">تورها</NuxtLink></li>
-        <li><NuxtLink to="/about-us">درباره ما</NuxtLink></li>
-        <li><NuxtLink to="/contact-us">تماس با ما</NuxtLink></li>
+        <li><NuxtLink active-class="text-rose-500" to="/">صفحه اصلی</NuxtLink></li>
+        <li><NuxtLink active-class="text-rose-500" to="/tour-list">تورها</NuxtLink></li>
+        <li><NuxtLink active-class="text-rose-500" to="/about-us">درباره ما</NuxtLink></li>
+        <li><NuxtLink active-class="text-rose-500" to="/contact-us">تماس با ما</NuxtLink></li>
       </ul>
       <NuxtLink to="/">
         <NuxtImg class="h-10" src="assets/img/logo.svg" />
