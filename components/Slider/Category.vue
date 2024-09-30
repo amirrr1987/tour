a8c527e8a1601d2f559222fd1736d04e2f337d05
Amir Maghami <Maghami.A1987@gmail.com>
2024-09-30 16:12:06 +0330

    Update Category.vue

--- components/Slider/Category.vue
+++ components/Slider/Category.vue
@@ -1,76 +1,44 @@
-<script setup lang="ts">
-import { Navigation, Pagination } from "swiper/modules";
-
-import "swiper/css";
-import "swiper/css/navigation";
-
-const list = [
-  "assets/img/hero/hero_bg_1_1.jpg",
-  "assets/img/hero/hero_bg_1_2.jpg",
-  "assets/img/hero/hero_bg_1_3.jpg",
-  "assets/img/hero/hero_bg_2_1.jpg",
-  "assets/img/hero/hero_bg_2_2.jpg",
-  "assets/img/hero/hero_bg_2_3.jpg",
-  "assets/img/hero/hero_bg_3_1.jpg",
-  "assets/img/hero/hero_bg_3_2.jpg",
-  "assets/img/hero/hero_bg_3_3.jpg",
-  "assets/img/hero/hero_bg_3_4.jpg",
-  "assets/img/hero/hero_bg_3_5.jpg",
-];
-</script>
-
 <template>
-  <header class="slider-category">
-    <Swiper
-      :modules="[SwiperAutoplay, SwiperEffectCreative, Navigation, Pagination]"
-      :slides-per-view="1"
-      :loop="true"
-      :effect="'creative'"
-      :pagination="true"
-      :navigation="true"
-      :autoplay="{
-        delay: 8000,
-        disableOnInteraction: true,
-      }"
-      :creative-effect="{
-        prev: {
-          shadow: false,
-          translate: ['-20%', 0, -1],
-        },
-        next: {
-          translate: ['100%', 0, 0],
-        },
-      }"
-    >
-      <SwiperSlide v-for="slide in list" :key="slide">
-        <NuxtImg :src="slide" />
-      </SwiperSlide>
-    </Swiper>
-  </header>
+  <swiper
+    :slidesPerView="3"
+    :spaceBetween="30"
+    :pagination="{
+      clickable: true,
+    }"
+    :modules="modules"
+    class="mySwiper"
+    dir="rtl"
+  >
+    <swiper-slide>Slide 1</swiper-slide>
+    <swiper-slide>Slide 2</swiper-slide><swiper-slide>Slide 3</swiper-slide>
+    <swiper-slide>Slide 4</swiper-slide><swiper-slide>Slide 5</swiper-slide>
+    <swiper-slide>Slide 6</swiper-slide><swiper-slide>Slide 7</swiper-slide>
+    <swiper-slide>Slide 8</swiper-slide><swiper-slide>Slide 9</swiper-slide>
+  </swiper>
 </template>
+<script>
+  // Import Swiper Vue.js components
+  import { Swiper, SwiperSlide } from 'swiper/vue';
 
-<style>
-.slider-category .swiper {
-  width: 100%;
-  height: 30rem;
-  direction: ltr;
-}
+  // Import Swiper styles
+  import 'swiper/css';
 
-.slider-category .swiper-slide {
-  text-align: center;
-  font-size: 18px;
-  background: #fff;
+  import 'swiper/css/pagination';
 
-  /* Center slide text vertically */
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
+  // import './style.css';
 
-.slider-category .swiper-slide img {
-  display: block;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-}
-</style>
+  // import required modules
+  import { Pagination } from 'swiper/modules';
+
+  export default {
+    components: {
+      Swiper,
+      SwiperSlide,
+    },
+    setup() {
+      return {
+        modules: [Pagination],
+      };
+    },
+  };
+</script>
