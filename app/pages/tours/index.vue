c73ae3338b8040948e04a9ee033233ba557844ff
Amir Maghami <Maghami.A1987@gmail.com>
2025-11-26 00:26:23 +0330

    Add 'count' field to tour filter form and update parameter handling

--- app/pages/tours/index.vue
+++ app/pages/tours/index.vue
@@ -26,6 +26,7 @@
     formData.value.endDate = ''
     formData.value.price = [0, 2000]
     formData.value.vehicle = ''
+    formData.value.count = 1
   }
   const setFilterInParams = () => {
     search.value = formData.value.search
@@ -36,6 +37,7 @@
     startDate.value = formData.value.startDate
     endDate.value = formData.value.endDate
     vehicle.value = formData.value.vehicle
+    count.value = formData.value.count
   }
   const search = useRouteQuery<string>('search', '')
   const category = useRouteQuery<string>('category', '')
@@ -45,6 +47,7 @@
   const vehicle = useRouteQuery<VehicleEnum | string>('vehicle', '')
   const startDate = useRouteQuery<string>('startDate', '')
   const endDate = useRouteQuery<string>('endDate', '')
+  const count = useRouteQuery<number>('count', 1)
   const onSubmit = () => {
     setFilterInParams()
   }
