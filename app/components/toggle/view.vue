5af977a0b032d72010fa7d6ea2281a5e226ba70d
Amir Maghami <Maghami.A1987@gmail.com>
2025-11-28 16:36:24 +0330

    Update view.vue

--- app/components/toggle/view.vue
+++ app/components/toggle/view.vue
@@ -1,19 +1,31 @@
 <script setup lang="ts">
-import type { ViewMode } from '~/models/app.model';
+  import type { ViewMode } from '~/models/app.model'
 
-const modelValue = defineModel<ViewMode>("viewMode", {
-  required: true,
-  default: "grid",
-});
+  const modelValue = defineModel<ViewMode>('viewMode', {
+    required: true,
+    default: 'grid'
+  })
 </script>
 <template>
   <div class="flex border rounded-lg overflow-hidden">
-    <UButton :variant="modelValue === 'grid' ? 'solid' : 'ghost'" color="neutral" size="sm" square
-      @click="modelValue = 'grid'">
+    <UButton
+      :variant="modelValue === 'grid' ? 'solid' : 'ghost'"
+      color="neutral"
+      size="sm"
+      square
+      @click="modelValue = 'grid'"
+      class="cursor-pointer"
+    >
       <UIcon name="i-lucide-grid" class="w-4 h-4" />
     </UButton>
-    <UButton :variant="modelValue === 'list' ? 'solid' : 'ghost'" color="neutral" size="sm" square
-      @click="modelValue = 'list'">
+    <UButton
+      :variant="modelValue === 'list' ? 'solid' : 'ghost'"
+      color="neutral"
+      size="sm"
+      square
+      @click="modelValue = 'list'"
+      class="cursor-pointer"
+    >
       <UIcon name="i-lucide-list" class="w-4 h-4" />
     </UButton>
   </div>
