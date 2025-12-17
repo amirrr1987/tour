044698b3b2e9883b9e1087867d5e75f55ffecebc
Amir Maghami <Maghami.A1987@gmail.com>
2025-12-17 21:02:21 +0330

    Update app.vue

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,15 @@
+<script setup lang="ts">
+  import type { Locale, Messages } from '@nuxt/ui'
+  import { en, fa_ir } from '@nuxt/ui/locale'
+
+  const { locale: i18nLocale } = useI18n()
+
+  const locale = computed<Locale<Messages>>(() => {
+    return i18nLocale.value === 'fa' ? fa_ir : en
+  })
+</script>
 <template>
-  <UApp class="bg-gray-100 dark:bg-gray-900">
+  <UApp class="bg-gray-100 dark:bg-gray-900" :locale="locale" :dir="locale.dir">
     <NuxtRouteAnnouncer />
     <NuxtLayout>
       <NuxtPage />
