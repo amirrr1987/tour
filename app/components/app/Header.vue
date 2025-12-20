56c737dafdb4d6566ec14878de2a7dcebb152014
Amir Maghami <Maghami.A1987@gmail.com>
2025-12-20 18:43:46 +0330

    Update Header.vue

--- app/components/app/Header.vue
+++ app/components/app/Header.vue
@@ -1,74 +1,85 @@
 <script setup lang="ts">
-  import type { NavigationMenuItem } from '@nuxt/ui'
-  import { en, fa_ir } from '@nuxt/ui/locale'
+import type { NavigationMenuItem } from "@nuxt/ui";
+import { en, fa_ir } from "@nuxt/ui/locale";
 
-  const { t } = useI18n()
-  const items = computed<NavigationMenuItem[]>(() => [
-    {
-      label: t('home.title'),
-      icon: 'i-lucide-home',
-      to: '/'
-    },
-    {
-      label: t('tours.title'),
-      icon: 'i-lucide-calendar-check',
-      to: '/tours'
-    },
-    {
-      label: t('aboutUs.title'),
-      icon: 'i-lucide-info',
-      to: '/about-us'
-    },
-    {
-      label: t('contactUs.title'),
-      icon: 'i-lucide-phone',
-      to: '/contact-us'
-    }
-  ])
-  const switchLocalePath = useSwitchLocalePath()
+const { t } = useI18n();
+const items = computed<NavigationMenuItem[]>(() => [
+  {
+    label: t("home.title"),
+    icon: "i-lucide-home",
+    to: "/",
+  },
+  {
+    label: t("tours.title"),
+    icon: "i-lucide-calendar-check",
+    to: "/tours",
+  },
+  {
+    label: t("aboutUs.title"),
+    icon: "i-lucide-info",
+    to: "/about-us",
+  },
+  {
+    label: t("contactUs.title"),
+    icon: "i-lucide-phone",
+    to: "/contact-us",
+  },
+]);
+const switchLocalePath = useSwitchLocalePath();
 
-  const locale = ref('fa-IR')
-  const handleLocaleChange = (loc: 'en' | 'fa-IR') => {
-    if (loc === 'en') {
-      navigateTo(switchLocalePath('en'))
-      window.document.documentElement.lang = 'en'
-      window.document.dir = 'ltr'
-    }
-    if (loc === 'fa-IR') {
-      navigateTo(switchLocalePath('fa'))
-      window.document.documentElement.lang = 'fa'
-      window.document.dir = 'rtl'
-    }
+const locale = ref("fa-IR");
+const handleLocaleChange = (loc: "en" | "fa-IR") => {
+  if (loc === "en") {
+    navigateTo(switchLocalePath("en"));
+    window.document.documentElement.lang = "en";
+    window.document.dir = "ltr";
+  } else {
+    navigateTo(switchLocalePath("fa"));
+    window.document.documentElement.lang = "fa";
+    window.document.dir = "rtl";
   }
-  onMounted(() => {
-    if (window.document.documentElement.lang === 'en') {
-      locale.value = 'en'
-    }
-    if (window.document.documentElement.lang === 'fa') {
-      locale.value = 'fa-IR'
-    }
-  })
+};
+onMounted(() => {
+  if (window.document.documentElement.lang === "en") {
+    locale.value = "en";
+    window.document.documentElement.dir = "ltr";
+  } else {
+    locale.value = "fa-IR";
+    window.document.documentElement.dir = "rtl";
+  }
+});
 </script>
 <template>
   <UHeader>
     <template #title>
       <Logo class="h-11 w-auto fill-primary" />
-      <span class="text-2xl font-bold text-primary hidden md:block">TourCode</span>
+      <span class="text-2xl font-bold text-primary hidden md:block"
+        >TourCode</span
+      >
     </template>
 
     <UNavigationMenu :items="items" />
 
     <template #right>
-      <UButton color="primary" variant="soft" to="/tours" icon="i-lucide-calendar-check">
-        <span class="hidden md:block">{{ $t('components.header.bookNow') }}</span>
+      <UButton
+        color="primary"
+        variant="soft"
+        to="/tours"
+        icon="i-lucide-calendar-check"
+      >
+        <span class="hidden md:block">{{
+          $t("components.header.bookNow")
+        }}</span>
       </UButton>
       <UColorModeButton class="cursor-pointer" />
 
       <ULocaleSelect
         v-model="locale"
-        @update:model-value="(value: string) => handleLocaleChange(value as 'en' | 'fa-IR')"
         :locales="[fa_ir, en]"
         class="w-30"
+        @update:model-value="
+          (value: string) => handleLocaleChange(value as 'en' | 'fa-IR')
+        "
       />
     </template>
 
